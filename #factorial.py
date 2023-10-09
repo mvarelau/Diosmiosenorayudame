3e174d9c4a812a98949a1674ff22b43a4432d66b
mvarelau <141885396+mvarelau@users.noreply.github.com>
2023-10-09 13:11:30 -0500

    Punto 5

---
+++ #factorial.py
@@ -0,0 +1,16 @@
+n = int(input("Ingrese un número natural para calcular su factorial: "))
+factorial = 1
+while n > 1:
+     factorial *= n
+     n -= 1
+print(f"El numero factorial de{n} es {factorial}")
+
+
+
+
+
+
+
+
+
+
