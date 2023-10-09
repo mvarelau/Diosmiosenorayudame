c374fe712be0f1e377f2c21d7a9cd26d9dee337f
mvarelau <141885396+mvarelau@users.noreply.github.com>
2023-10-09 12:55:14 -0500

    Punto 2

---
+++ #chevereynotanchevere.py
@@ -0,0 +1,25 @@
+#chevereynotanchevere
+def impares():
+    a = 1
+    print("Esta es la lista de numeros impares")
+    while a <= 999:
+        if (a % 2) != 0:
+            print(a)
+        a += 1
+def pares():
+    a = 2
+    print("Esta es la lista de numeros pares")
+    while a <= 1000:
+        if (a % 2) == 0:
+            print(a)
+        a += 1
+
+impares()
+pares()
+
+
+
+    
+
+
+
